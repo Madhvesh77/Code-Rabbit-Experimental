126ad006b59ac424bd571d2e7834817b132628b9
madhvesh-77 <madhveshs@sahaj.ai>
2024-10-27 23:21:55 +0530

    initial commit new branch 2

--- sample.py
+++ sample.py
@@ -1,5 +1,13 @@
-import math
+def add_two_numbers(first_number, second_number):
+    """
+    Adds two numbers and returns the result.
 
-def add_numbers(a, b):
-    sum = a + b
-    return sum
+    Parameters:
+    first_number (int): The first number to add.
+    second_number (int): The second number to add.
+
+    Returns:
+    int: The sum of first_number and second_number.
+    """
+    result = first_number + second_number
+    return result
